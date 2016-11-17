8995c14787f9f7794654350c9685bbbd7bd8fd1b
Daniel Song <dan.songh@gmail.com>
2016-11-16 18:48:24 -0800

    got rid of some stuffz

--- Lazy Guitar/ChordView+CoreDataProperties.swift
+++ Lazy Guitar/ChordView+CoreDataProperties.swift
@@ -1,11 +1,3 @@
-//
-//  ChordView+CoreDataProperties.swift
-//  Lazy Guitar
-//
-//  Created by Daniel Song on 11/7/16.
-//  Copyright © 2016 Daniel Song. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
--- Lazy Guitar/Title+CoreDataClass.swift
+++ Lazy Guitar/Title+CoreDataClass.swift
@@ -1,11 +1,3 @@
-//
-//  Title+CoreDataClass.swift
-//  Lazy Guitar
-//
-//  Created by Daniel Song on 11/3/16.
-//  Copyright © 2016 Daniel Song. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
