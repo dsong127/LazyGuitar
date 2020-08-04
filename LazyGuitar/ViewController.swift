6fcbb167c97cd4e206d53962d00a37be41634301
dsong127 <dsong127@gmail.com>
2020-08-04 12:11:04 -0700

    Update gitignore

---
+++ LazyGuitar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LazyGuitar
+//
+//  Created by Daniel Song on 6/25/19.
+//  Copyright © 2019 Daniel Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
